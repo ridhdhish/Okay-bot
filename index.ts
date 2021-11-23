7943980dda74e30d40317f8a81eab7dca48d669c
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-11-23 14:20:08 +0530

    simple calc with discord commands

--- index.ts
+++ index.ts
@@ -12,14 +12,6 @@ const client = new DiscordJS.Client({
 client.on("ready", () => {
   console.log("Bot is ready to roll");
 
-  // FIXME:
-  // commands(client, "ping", (message) => {
-  //   message.reply({
-  //     content: "pong",
-  //     ephemeral: true,
-  //   });
-  // });
-
   userCommands(client);
 
   // FIXME: Solve error first
@@ -27,89 +19,5 @@ client.on("ready", () => {
     // console.log(commands.guild);
   });
 });
-
-// FIXME:
-// const client = new DiscordJS.Client({
-//   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-// });
-
-// client.on("ready", () => {
-//   console.log("Bot is ready to roll!!");
-
-//   // Only for a single guild/server
-//   const guildId = "750218825680551987";
-//   const guild = client.guilds.cache.get(guildId);
-//   let commands;
-
-//   if (guild) {
-//     commands = guild.commands;
-//   } else {
-//     commands = client.application?.commands;
-//   }
-
-//   /*
-//     Creating new command
-//     These slash commands are listen to default discord commands like /tenor, /giphy
-//   */
-//   commands?.create({
-//     name: "ping",
-//     description: "Replies with pong",
-//   });
-
-//   commands?.create({
-//     name: "add",
-//     description: "Add two numbers",
-//     options: [
-//       {
-//         name: "number1",
-//         description: "First number",
-//         required: true,
-//         type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-//       },
-//       {
-//         name: "number2",
-//         description: "Second number",
-//         required: true,
-//         type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-//       },
-//     ],
-//   });
-// });
-
-// // Listening to user interaction event
-// client.on("interactionCreate", async (interation) => {
-//   if (!interation.isCommand()) {
-//     return;
-//   }
-
-//   if (interation.commandName === "ping") {
-//     interation.reply({
-//       content: "pong",
-//       ephemeral: true,
-//     });
-//   } else if (interation.commandName === "add") {
-//     const num1 = interation.options.getNumber("number1")!;
-//     const num2 = interation.options.getNumber("number2")!;
-
-//     await interation.deferReply({
-//       ephemeral: true,
-//     });
-
-//     await new Promise((resolve) => setTimeout(resolve, 5000));
-
-//     interation.editReply({
-//       content: `${num1} + ${num2} = ${num1 + num2}`,
-//     });
-//   }
-// });
-
-// On message creation event
-// client.on("messageCreate", (message) => {
-//   if (message.content.toLowerCase() === "ping") {
-//     message.reply({
-//       content: "pong",
-//     });
-//   }
-// });
 
 client.login(process.env.TOKEN);
--- src/interactions.ts
+++ src/interactions.ts
@@ -5,8 +5,6 @@ export default (client: Client, callback: Function) => {
   const guild = client.guilds.cache.get(guildId);
   let commands: GuildApplicationCommandManager | undefined;
 
-  console.log(`Type of commands: ${typeof guild?.commands}`);
-
   commands = guild?.commands;
 
   // if (guild) {
@@ -15,14 +13,70 @@ export default (client: Client, callback: Function) => {
   //   commands = client.application?.commands || null;
   // }
 
+  // Pinging
   commands?.create({
     name: "ping",
     description: "Reply with pong.",
   });
 
+  // Addition command
   commands?.create({
     name: "add",
     description: "Add two numbers",
+    options: [
+      {
+        name: "number1",
+        description: "First Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+      {
+        name: "number2",
+        description: "Second Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+    ],
+  });
+
+  // Subtract command
+  commands?.create({
+    name: "sub",
+    description: "Subtract two numbers",
+    options: [
+      {
+        name: "number1",
+        description: "First Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+      {
+        name: "number2",
+        description: "Second Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+    ],
+  });
+
+  // Multiplication command
+  commands?.create({
+    name: "mul",
+    description: "Multiply two numbers",
+    options: [
+      {
+        name: "number1",
+        description: "First Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+      {
+        name: "number2",
+        description: "Second Number",
+        type: Constants.ApplicationCommandOptionTypes.NUMBER,
+      },
+    ],
+  });
+
+  // Division command
+  commands?.create({
+    name: "div",
+    description: "Divide two numbers",
     options: [
       {
         name: "number1",
@@ -57,6 +111,39 @@ export default (client: Client, callback: Function) => {
 
       interaction.editReply({
         content: `${num1} + ${num2} = ${num1 + num2}`,
+      });
+    } else if (interaction.commandName === "sub") {
+      const num1 = interaction.options.getNumber("number1")!;
+      const num2 = interaction.options.getNumber("number2")!;
+
+      await interaction.deferReply({
+        ephemeral: true,
+      });
+
+      interaction.editReply({
+        content: `${num1} - ${num2} = ${num1 - num2}`,
+      });
+    } else if (interaction.commandName === "mul") {
+      const num1 = interaction.options.getNumber("number1")!;
+      const num2 = interaction.options.getNumber("number2")!;
+
+      await interaction.deferReply({
+        ephemeral: true,
+      });
+
+      interaction.editReply({
+        content: `${num1} * ${num2} = ${num1 * num2}`,
+      });
+    } else if (interaction.commandName === "div") {
+      const num1 = interaction.options.getNumber("number1")!;
+      const num2 = interaction.options.getNumber("number2")!;
+
+      await interaction.deferReply({
+        ephemeral: true,
+      });
+
+      interaction.editReply({
+        content: `${num1} / ${num2} = ${num1 / num2}`,
       });
     }
   });
